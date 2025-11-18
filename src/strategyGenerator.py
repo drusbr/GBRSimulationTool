2ef83608fad2226202c2260dc57cfb981c9f2da2
drusbr <daviruschel9@gmail.com>
2025-11-18 12:55:41 +0000

    Add function description for strategy generation
    Added a function description for generating random strategies.

---
+++ src/strategyGenerator.py
@@ -0,0 +1,3 @@
+# This function should be able to generate random strategies, with a starting position and length (all in meters, in terms of cumulative distance along the track)
+# It should return the strategy as follows: for example, a 2-acceleration strategy will output: [(0, 100), (500, 50)] where the first value is initial cumulative distance, and the second value the length of acceleration
+# Feel free to explore different ideas of generating strategies
