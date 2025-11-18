b0d5ea985821a91889da6daa28bb3f51e9500ee8
drusbr <daviruschel9@gmail.com>
2025-11-18 12:53:52 +0000

    Update trackLoad.py

--- src/trackLoad.py
+++ src/trackLoad.py
@@ -1,1 +1,5 @@
+# This function should be able to receive track coordinates (in both Cartesian and/or Latitude/Longitude)
+# It should calculate the distance between each point along the track, and consequently calculate  the distance of the lap (i.e. sum of all distances between points)
+# It should calculate the elevation gradient at each point, with reasoning behind the choice of lookahead distance
+# It should calculate the curvature at each point, and similarly to above, with reasoning behind choice of lookahead distance
 
